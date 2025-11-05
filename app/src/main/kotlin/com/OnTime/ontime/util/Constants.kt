557ff032b2ab763995201a2c3da4e770040ca5b9
isaac8570 <isaac8570@gachon.ac.kr>
2025-11-06 01:07:26 +0900

    feat: 구글 제미나이 모델 연동 세팅

--- app/src/main/kotlin/com/OnTime/ontime/util/Constants.kt
+++ app/src/main/kotlin/com/OnTime/ontime/util/Constants.kt
@@ -4,7 +4,9 @@ object Constants {
     
     // API Keys
     const val GOOGLE_MAPS_API_KEY = "YOUR_GOOGLE_MAPS_API_KEY"
-    const val GEMINI_API_KEY = "YOUR_GEMINI_API_KEY"
+    import com.OnTime.ontime.BuildConfig
+
+    const val GEMINI_API_KEY = BuildConfig.GEMINI_API_KEY
     
     // Transport Modes
     const val MODE_TRANSIT = "transit"
